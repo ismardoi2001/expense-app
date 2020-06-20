c38fbc5a20daf193fa1a6654d2e0b61386e423c2
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-20 12:21:35 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Child from './Child.js';
+import Child from './Child.js'
 
 function App() {
   return (
--- src/Child.js
+++ src/Child.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Child() {
 return (
-    <div className ="container">
+    <div className ="contaner">
     <h1 className="text-center">Expense Tracker</h1>
     <h3>Your Balance<br/>$260</h3>
     <div className ="expense-container">
@@ -12,19 +12,34 @@ return (
 
      <h3>History</h3>
      <hr/>
+    <ul className="transaction-list">
+    <li>
+    <span>Cash</span>
+    <span>+500</span>
+    </li>
+    <li>
+    <span>Cash</span>
+    <span>+500</span>
+    </li>
+    <li>
+    <span>Cash</span>
+    <span>+500</span>
+    </li>
+    </ul>
 
      <h3>Add New Transcation</h3>
      <hr/>
-     <form>
+     <form className="transation-form">
         <label>
             Enter Description <br/>
-            <input type="text"/>
+            <input type="text" required/>
         </label>
-
+        <br/>
         <label>
             Enter Amount<br/>
-            <input type="number"/>
+            <input type="number" required/>
         </label>
+        <br/>
         <input type="submit" value="Add Trnsaction"></input>
      </form>
      </div>
