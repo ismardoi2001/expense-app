a4e4865e7467a658280e9a95ab1b17e8d4c8b6eb
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-21 15:04:33 +0500

    update task

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -25,14 +25,14 @@ export const AddTransaction = () => {
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Product Name" />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
-            (negative - expense, positive - income)</label
+            (Negative - Expense, Positive - Income)</label
           >
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter  Product amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
