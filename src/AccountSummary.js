705d3a561b295017ca92ef1f685cd53da93f4ba9
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-21 21:25:44 +0500

    update almost completed task

--- src/AccountSummary.js
+++ src/AccountSummary.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { GlobalContext } from './GlobalState';
 
 export const AccountSummary = () => {
-    
+ const { transactions } = useContext(GlobalContext);
+
+ const amounts = transactions.map(transaction => transaction.amount);
+
+ const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+ const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
     return (
         <div className="inc-exp-container">            
         <div>
         <h4>Income</h4>
         <p className ="money plus">
-        +0.00
+        {income}
         </p>
         </div>     
         <div>
         <h4>Expesne</h4>
         <p className ="money minus">
-        -0.00
+        {expense}
         </p>
         </div>    
         </div> 
